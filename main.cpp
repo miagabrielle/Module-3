d4774da7bcb6953f3c05a57214936202fd609c52
Mia Garcia <0942176@my.scccd.edu>
2025-02-20 23:03:50 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,106 +1,25 @@
 #include <iostream>
 using namespace std;
 
-
-struct Node {
-    int data;
-    Node* next;
-
-   
-    Node(int value) {
-        data = value;
-        next = nullptr;
-    }
-};
-
-
-void insertAtEnd(Node*& head, int value) {
-    Node* newNode = new Node(value);
-
-    
-    if (head == nullptr) {
-        head = newNode;
-    } else {
-       
-        Node* temp = head;
-        while (temp->next != nullptr) {
-            temp = temp->next;
-        }
-        
-        temp->next = newNode;
-    }
-}
-
-
-void deleteNodeByValue(Node*& head, int value) {
-    // If the list is empty
-    if (head == nullptr) {
-        cout << "The list is empty." << endl;
-        return;
-    }
-
-    
-    if (head->data == value) {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-        return;
-    }
-
-    
-    Node* temp = head;
-    while (temp->next != nullptr && temp->next->data != value) {
-        temp = temp->next;
-    }
-
-    
-    if (temp->next == nullptr) {
-        cout << "Value " << value << " not found in the list." << endl;
-        return;
-    }
-
-    
-    Node* nodeToDelete = temp->next;
-    temp->next = temp->next->next;
-    delete nodeToDelete;
-}
-
-
-void displayList(Node* head) {
-    if (head == nullptr) {
-        cout << "The list is empty." << endl;
-        return;
-    }
-
-    Node* temp = head;
-    while (temp != nullptr) {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-    cout << endl;
-}
-
 int main() {
-    Node* head = nullptr;
-
+    // Part 1: Pointers and Variables
+    int a = 10;
+    int *p = &a;
     
-    insertAtEnd(head, 10);
-    insertAtEnd(head, 20);
-    insertAtEnd(head, 30);
-    insertAtEnd(head, 40);
-
+    std::cout << "Address stored in pointer p: " << p << std::endl;
+    std::cout << "Value of a using pointer p: " << *p << std::endl;
     
-    cout << "Linked List: ";
-    displayList(head);
-
-   
-    deleteNodeByValue(head, 20);
-    cout << "After deleting 20, Linked List: ";
-    displayList(head);
-
-    deleteNodeByValue(head, 50);
-    cout << "After attempting to delete 50, Linked List: ";
-    displayList(head);
-
+    *p = 20;
+    std::cout << "New value of a: " << a << std::endl;
+    
+    
+    // Part 2: Pointer Arithmetic and Arrays
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *prt = arr;
+    
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Address of arr[" << i << "]: " << (p+i) << endl;
+        std::cout << "Value of arr[" <<i << "]: " << *(p+i) << endl;
+    }
     return 0;
 }
